30d7d64145bcb3cb68d412e42b944bb334cafcba
Joao <1994meyer@gmail.com>
2017-06-06 08:55:48 -0300

    reformatted code

--- gol.c
+++ gol.c
@@ -10,30 +10,26 @@
 * In this version, a 2D array of ints is used.  A 1 cell is on, a 0 cell is off.
 * The game plays a number of steps (given by the input), printing to the screen each time.  'x' printed
 * means on, space means off.
-*
 */
 #include <stdio.h>
 #include <stdlib.h>
 typedef unsigned char cell_t;
 
-cell_t ** allocate_board (int size) {
-  cell_t ** board = (cell_t **) malloc(sizeof(cell_t*)*size);
-  int	i;
-  for (i=0; i<size; i++)
-  board[i] = (cell_t *) malloc(sizeof(cell_t)*size);
+cell_t ** allocate_board(int size) {
+  cell_t** board = (cell_t**) malloc(sizeof(cell_t*) * size);
+  for (int i = 0; i < size; i++)
+    board[i] = (cell_t *) malloc(sizeof(cell_t) * size);
   return board;
 }
 
-void free_board (cell_t ** board, int size) {
-  int     i;
-  for (i=0; i<size; i++)
-  free(board[i]);
+void free_board(cell_t ** board, int size) {
+  for (int i = 0; i < size; i++)
+    free(board[i]);
   free(board);
 }
 
-
 /* return the number of on cells adjacent to the i,j cell */
-int adjacent_to (cell_t ** board, int size, int i, int j) {
+int adjacent_to(cell_t ** board, int size, int i, int j) {
   int	k, l, count=0;
 
   int sk = (i>0) ? i-1 : i;
@@ -49,72 +45,71 @@ int adjacent_to (cell_t ** board, int size, int i, int j) {
   return count;
 }
 
-void play (cell_t ** board, cell_t ** newboard, int size) {
-  int	i, j, a;
+void play cell_t ** board, cell_t ** newboard, int size) {
+  int	a;
   /* for each cell, apply the rules of Life */
-  for (i=0; i<size; i++)
-  for (j=0; j<size; j++) {
-    a = adjacent_to (board, size, i, j);
-    if (a == 2) newboard[i][j] = board[i][j];
-    if (a == 3) newboard[i][j] = 1;
-    if (a < 2) newboard[i][j] = 0;
-    if (a > 3) newboard[i][j] = 0;
-  }
+  for (int i = 0; i < size; i++)
+    for (int j = 0; j < size; j++) {
+      a = adjacent_to(board, size, i, j);
+      if (a == 2)
+        newboard[i][j] = board[i][j];
+      if (a == 3)
+        newboard[i][j] = 1;
+      if (a < 2)
+        newboard[i][j] = 0;
+      if (a > 3)
+        newboard[i][j] = 0;
+    }
 }
 
 /* print the life board */
-void print (cell_t ** board, int size) {
-  int	i, j;
+void print(cell_t ** board, int size) {
   /* for each row */
-  for (j=0; j<size; j++) {
+  for (int j = 0; j < size; j++) {
     /* print each column position... */
-    for (i=0; i<size; i++)
-    printf ("%c", board[i][j] ? 'x' : ' ');
+    for (int i = 0; i < size; i++)
+      printf ("%c", board[i][j] ? 'x' : ' ');
     /* followed by a carriage return */
     printf ("\n");
   }
 }
 
 /* read a file into the life board */
-void read_file (FILE * f, cell_t ** board, int size) {
-  int	i, j;
-  char	*s = (char *) malloc(size+10);
-
+void read_file(FILE * f, cell_t ** board, int size) {
+  char* s = (char*) malloc(size + 10);
   /* read the first new line (it will be ignored) */
-  fgets (s, size+10,f);
+  fgets(s, size + 10, f);
 
   /* read the life board */
-  for (j=0; j<size; j++) {
+  for (int j = 0; j < size; j++) {
     /* get a string */
-    fgets (s, size+10,f);
+    fgets (s, size + 10, f);
     /* copy the string to the life board */
-    for (i=0; i<size; i++)
-    board[i][j] = s[i] == 'x';
-
+    for (int i = 0; i < size; i++)
+      board[i][j] = s[i] == 'x';
   }
 }
 
 int main () {
   int size, steps;
-  FILE    *f;
+  FILE* f;
   f = stdin;
-  fscanf(f,"%d %d", &size, &steps);
-  cell_t ** prev = allocate_board (size);
-  read_file (f, prev,size);
+  fscanf(f, "%d %d", &size, &steps);
+  cell_t** prev = allocate_board(size);
+  read_file(f, prev,size);
   fclose(f);
-  cell_t ** next = allocate_board (size);
-  cell_t ** tmp;
-  int i;
+  cell_t** next = allocate_board(size);
+  cell_t** tmp;
   #ifdef DEBUG
-  printf("Initial:\n");
-  print(prev,size);
+    printf("Initial:\n");
+    print(prev, size);
   #endif
 
-  for (i=0; i<steps; i++) {
-    play (prev,next,size);
+  for (int i = 0; i < steps; i++) {
+    play(prev, next, size);
     #ifdef DEBUG
-    printf("%d ----------\n", i + 1);
-    print (next,size);
+      printf("%d ----------\n", i + 1);
+      print(next, size);
     #endif
     tmp = next;
     next = prev;
