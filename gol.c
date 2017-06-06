f353397cb4e50031f5eb4226f30f922815cc30ac
Joao <1994meyer@gmail.com>
2017-06-06 09:29:19 -0300

    minor changes

--- gol.c
+++ gol.c
@@ -1,51 +1,94 @@
-/*
-* The Game of Life
-*
-* a cell is born, if it has exactly three neighbours
-* a cell dies of loneliness, if it has less than two neighbours
-* a cell dies of overcrowding, if it has more than three neighbours
-* a cell survives to the next generation, if it does not die of loneliness
-* or overcrowding
-*
-* In this version, a 2D array of ints is used.  A 1 cell is on, a 0 cell is off.
-* The game plays a number of steps (given by the input), printing to the screen each time.  'x' printed
-* means on, space means off.
-*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <mpi.h>
 typedef unsigned char cell_t;
 
-cell_t ** allocate_board(int size) {
+// prototypes
+cell_t** allocate_board(int size);
+void free_board(cell_t** board, int size);
+inline int adjacent_to(cell_t** board, int size, int i, int j);
+void play(cell_t** board, cell_t** newboard, int size);
+void print(cell_t** board, int size);
+void read_file(FILE* f, cell_t** board, int size);
+
+int main () {
+  int size, steps;
+  FILE* f;
+  f = stdin;
+  fscanf(f, "%d %d", &size, &steps);
+  cell_t** prev = allocate_board(size);
+  read_file(f, prev,size);
+  fclose(f);
+  cell_t** next = allocate_board(size);
+  cell_t** tmp;
+
+  #ifdef DEBUG
+    printf("Initial:\n");
+    print(prev, size);
+  #endif
+
+  for (int i = 0; i < steps; i++) {
+    play(prev, next, size);
+    #ifdef DEBUG
+      printf("%d ----------\n", i + 1);
+      print(next, size);
+    #endif
+    tmp = next;
+    next = prev;
+    prev = tmp;
+  }
+
+  #ifdef RESULT
+    printf("Final:\n");
+    print (prev,size);
+  #endif
+
+  free_board(prev,size);
+  free_board(next,size);
+}
+
+cell_t** allocate_board(int size) {
   cell_t** board = (cell_t**) malloc(sizeof(cell_t*) * size);
   for (int i = 0; i < size; i++)
     board[i] = (cell_t *) malloc(sizeof(cell_t) * size);
   return board;
 }
 
-void free_board(cell_t ** board, int size) {
+void free_board(cell_t** board, int size) {
   for (int i = 0; i < size; i++)
     free(board[i]);
   free(board);
 }
 
 /* return the number of on cells adjacent to the i,j cell */
-int adjacent_to(cell_t ** board, int size, int i, int j) {
-  int	k, l, count=0;
+inline int adjacent_to(cell_t** board, int size, int i, int j) {
+  int count = 0;
+  if (i-1 > 0) {
+    count += board[i-1][j];
+    if (j - 1 > 0)
+      count += board[i-1][j-1];
+    if (j+1 < size)
+      count += board[i-1][j+1];
+  }
 
-  int sk = (i>0) ? i-1 : i;
-  int ek = (i+1 < size) ? i+1 : i;
-  int sl = (j>0) ? j-1 : j;
-  int el = (j+1 < size) ? j+1 : j;
+  if (j-1 > 0) {
+    count += board[i][j-1];
+    if (i+1 < size)
+      count += board[i+1][j-1];
+  }
 
-  for (k=sk; k<=ek; k++)
-  for (l=sl; l<=el; l++)
-  count+=board[k][l];
-  count-=board[i][j];
+  if (i+1 < size) {
+    count += board[i+1][j];
+    if (j+1 < size)
+      count += board[i+1][j+1];
+  }
 
+  if (j+1 < size)
+    count += board[i][j+1];
   return count;
 }
 
-void play cell_t ** board, cell_t ** newboard, int size) {
+void play(cell_t** board, cell_t** newboard, int size) {
   int	a;
   /* for each cell, apply the rules of Life */
   for (int i = 0; i < size; i++)
@@ -63,7 +106,7 @@ void play cell_t ** board, cell_t ** newboard, int size) {
 }
 
 /* print the life board */
-void print(cell_t ** board, int size) {
+void print(cell_t** board, int size) {
   /* for each row */
   for (int j = 0; j < size; j++) {
     /* print each column position... */
@@ -75,7 +118,7 @@ void print(cell_t ** board, int size) {
 }
 
 /* read a file into the life board */
-void read_file(FILE * f, cell_t ** board, int size) {
+void read_file(FILE* f, cell_t** board, int size) {
   char* s = (char*) malloc(size + 10);
   /* read the first new line (it will be ignored) */
   fgets(s, size + 10, f);
@@ -88,39 +131,4 @@ void read_file(FILE * f, cell_t ** board, int size) {
     for (int i = 0; i < size; i++)
       board[i][j] = s[i] == 'x';
   }
-}
-
-int main () {
-  int size, steps;
-  FILE* f;
-  f = stdin;
-  fscanf(f, "%d %d", &size, &steps);
-  cell_t** prev = allocate_board(size);
-  read_file(f, prev,size);
-  fclose(f);
-  cell_t** next = allocate_board(size);
-  cell_t** tmp;
-  #ifdef DEBUG
-    printf("Initial:\n");
-    print(prev, size);
-  #endif
-
-  for (int i = 0; i < steps; i++) {
-    play(prev, next, size);
-    #ifdef DEBUG
-      printf("%d ----------\n", i + 1);
-      print(next, size);
-    #endif
-    tmp = next;
-    next = prev;
-    prev = tmp;
-  }
-
-#ifdef RESULT
-  printf("Final:\n");
-  print (prev,size);
-#endif
-
-  free_board(prev,size);
-  free_board(next,size);
 }
