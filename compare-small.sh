461df0aef03701c6e524c81d636030c3d640798d
João Meyer <1994meyer@gmail.com>
2017-06-24 15:14:08 -0300

    two simple test scripts

---
+++ compare-small.sh
@@ -0,0 +1,32 @@
+printf "Starting test\n\n";
+
+function run_original {
+    cd original/;
+    make clean > /dev/null;
+    make > /dev/null;
+    time ./gol < ../input-little.in > ../original_out.txt;
+    make clean > /dev/null;
+    cd ..;
+}
+
+function run_distr {
+    make clean > /dev/null;
+    make > /dev/null;
+    time mpirun -np 4 ./gol < ./input-little.in > ./distr_out.txt;
+    make clean > /dev/null;
+}
+
+# run sequential
+printf "Running sequential first\n";
+run_original;
+
+printf "===================\n\n";
+
+# run distributed
+printf "Running distributed (np = 4)\n";
+run_distr;
+
+printf "===================\n\n";
+
+printf "Comparing results:\n(if no output, they are equal)\n";
+cmp ./original_out.txt ./distr_out.txt;
